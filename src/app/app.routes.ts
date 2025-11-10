994ca187063dc0c9f5dc6b967e909355d67c5983
aadkin200 <82674047+aadkin200@users.noreply.github.com>
2025-11-09 19:00:00 -0500

    refactorted code into component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Resume } from './components/resume/resume';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'resume' },
+  { path: 'resume', component: Resume },
+];
